c0757f492cc557bce7b6fdce5454904f88d21ae8
Priscila Nunes <priscilavespoli92@gmail.com>
2026-03-24 10:33:14 +0100

    feat-inventario ok

---
+++ assets/java/script.js
@@ -0,0 +1,20 @@
+const inventarioTienda = [
+  {
+    titulo: "Pokemon Yellow",
+    precio: 79.99,
+    esDigital: false,
+    generos: ["Aventura", "Acción", "Open World"]
+  },
+  {
+    titulo: "Mickey: Castle of Illusion",
+    precio: 69.99,
+    esDigital: true,
+    generos: ["Plataforma", "Aventura", "Acción"]
+  },
+  {
+    titulo: "Winning Eleven",
+    precio: 34.99,
+    esDigital: true,
+    generos: ["Simulación", "Deporte", "Aventura"]
+  }
+];
